0744f580b855312aff1264675f5ab994ec189127
Edgar J Guinan R <ejguinan@gmail.com>
2026-02-27 10:26:27 -0400

    Add payment gateway configuration for Stripe, PayPal, and bank transfer

---
+++ config/payments.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'stripe' => [
+        'api_key' => 'your_stripe_api_key',
+        'client_id' => 'your_stripe_client_id',
+    ],
+    'paypal' => [
+        'api_key' => 'your_paypal_api_key',
+        'client_id' => 'your_paypal_client_id',
+    ],
+    'bank_transfer' => [
+        'account_name' => 'Your Bank Account Name',
+        'account_number' => 'Your Bank Account Number',
+        'bank_name' => 'Your Bank Name',
+        'swift_code' => 'Your Bank Swift Code',
+    ],
+];
