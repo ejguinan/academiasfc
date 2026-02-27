677c770089fa6bebd38502212da80e137b2972cd
Edgar J Guinan R <ejguinan@gmail.com>
2026-02-27 10:26:07 -0400

    Create database configuration file for MySQL connection

---
+++ config/database.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'host' => 'localhost',
+    'port' => '3306',
+    'database' => 'your_database_name',
+    'username' => 'your_username',
+    'password' => 'your_password',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+];
